81f866ad9e6be7f57bbf0f1c22a7880aad3b4db2
Jon Hill <jon@jonhill.ca>
2017-07-07 13:02:26 +0200

    adding JWT_AUTH_RETURN_TUPLES to the project to make it appropriate for use with django-rest-framework based authentication

--- django_jwt_auth/backends/__init__.py
+++ django_jwt_auth/backends/__init__.py
@@ -4,18 +4,30 @@ from ..utils import jwt_to_user_dictionary
 
 class JSONWebTokenAuthenticationBackend(object):
     def authenticate(self, request):
+        return_tuples = settings.JWT_AUTH_RETURN_TUPLES if \
+                        hasattr(settings, 'JWT_AUTH_RETURN_TUPLES') else  False
+                        
         if not hasattr(settings, 'JWT_KEY'):
             raise EnvironmentError("You must set JWT_KEY in your settings file.")
             
         token = request.META.get('HTTP_AUTHORIZATION', None)
         if token is None:
-            return AnonymousUser()
+            if return_tuples:
+                return AnonymousUser(), None
+            else:
+                return AnonymousUser()
 
         ud = jwt_to_user_dictionary(token, settings.JWT_KEY)
         if 'pk' in ud:
-            return User.objects.get(pk=ud['pk'])
+            if return_tuples:
+                return User.objects.get(pk=ud['pk']), None
+            else:
+                return User.objects.get(pk=ud['pk'])
         else:
-            return AnonymousUser()
+            if return_tuples:
+                return AnonymousUser(), None
+            else:
+                return AnonymousUser()
     
     def get_user(self, user_id):
         try:
