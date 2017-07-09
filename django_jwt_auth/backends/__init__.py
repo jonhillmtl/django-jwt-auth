4f72e6f0ebbbc0449516c33086cbe4aaa5a10c46
Jon Hill <jon@jonhill.ca>
2017-07-09 12:12:13 +0200

    small cleanup of tuples code

--- django_jwt_auth/backends/__init__.py
+++ django_jwt_auth/backends/__init__.py
@@ -2,32 +2,30 @@ from django.conf import settings
 from django.contrib.auth.models import User, AnonymousUser
 from ..utils import jwt_to_user_dictionary
 
+def prepare_user(user):
+    return_tuples = settings.JWT_AUTH_RETURN_TUPLES if \
+                    hasattr(settings, 'JWT_AUTH_RETURN_TUPLES') else  False
+    
+    if return_tuples:
+        return User, None
+    else:
+        return User
+
+
 class JSONWebTokenAuthenticationBackend(object):
     def authenticate(self, request):
-        return_tuples = settings.JWT_AUTH_RETURN_TUPLES if \
-                        hasattr(settings, 'JWT_AUTH_RETURN_TUPLES') else  False
-                        
         if not hasattr(settings, 'JWT_KEY'):
             raise EnvironmentError("You must set JWT_KEY in your settings file.")
-            
+        
         token = request.META.get('HTTP_AUTHORIZATION', None)
         if token is None:
-            if return_tuples:
-                return AnonymousUser(), None
-            else:
-                return AnonymousUser()
-
+            return prepare_user(AnonymousUser())
+            
         ud = jwt_to_user_dictionary(token, settings.JWT_KEY)
         if 'id' in ud:
-            if return_tuples:
-                return User.objects.get(pk=ud['id']), None
-            else:
-                return User.objects.get(pk=ud['id'])
+            return prepare_user(User.objects.get(pk=ud['id']))
         else:
-            if return_tuples:
-                return AnonymousUser(), None
-            else:
-                return AnonymousUser()
+            return prepare_user(AnonymousUser())
     
     def get_user(self, user_id):
         try:
--- example/example/views.py
+++ example/example/views.py
@@ -7,7 +7,9 @@ from django_jwt_auth.utils import user_to_dictionary
 @csrf_exempt
 def test_auth(request):
     user = authenticate(request)
-    if isinstance(user, AnonymousUser):
+    if user is None:
+        return JsonResponse(dict(success=False, error='No user found (AnonymousUser)'))
+    elif isinstance(user, AnonymousUser):
         return JsonResponse(dict(success=False, error='No user found (AnonymousUser)'))
     else:
         return JsonResponse(dict(success=True, user=user_to_dictionary(user)))
