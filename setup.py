5e1364a742c8bb970c72b20d8a363b5528b4dcb5
Jon Hill <jon@jonhill.ca>
2017-07-13 11:37:17 +0200

    bumping version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='django-jwt-auth',
-      version='0.4',
+      version='0.5',
       description='Django JSON Web Token authentication backend',
       author='Jon Hill',
       author_email='jon@jonhill.ca',
